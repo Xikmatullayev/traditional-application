142a638310dd9af577df2632a33dedc8d4b5bfe4
Lazizbek <juniordev1005@gmail.com>
2021-07-10 23:02:58 +0500

    vue add (router, vuex, vuetify)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,134 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-icon @click="drawer = !drawer" color="red">mdi-circle</v-icon>
+      <v-icon color="yellow">mdi-circle</v-icon>
+      <v-icon color="green">mdi-circle</v-icon>
+
+      <v-spacer></v-spacer>
+      
+      <v-icon color="red">mdi-circle</v-icon>
+      <v-icon color="yellow">mdi-circle</v-icon>
+      <v-icon @click="drawerR = !drawerR" color="green">mdi-circle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar
+      app
+      clipped-right
+      flat
+      height="72"
+    >
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="156">
+        <v-text-field
+        background-color="grey lighten-2"
+        dense
+        flat
+        hide-details
+        rounded
+        solo
+        ></v-text-field>
+      </v-responsive>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      width="300"
+    >
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        color="grey lighten-3"
+        mini-variant
+      >
+        <v-avatar
+          class="d-block text-center mx-auto mt-4"
+          color="grey darken-1"
+          size="36"
+        ></v-avatar>
+
+        <v-divider class="mx-3 my-5"></v-divider>
+
+        <v-avatar
+          v-for="n in 6"
+          :key="n"
+          class="d-block text-center mx-auto mb-9"
+          color="grey lighten-1"
+          size="28"
+        ></v-avatar>
+      </v-navigation-drawer>
+
+      <v-sheet
+        color="grey lighten-5"
+        height="128"
+        width="100%"
+      ></v-sheet>
+
+      <v-list
+        class="pl-14"
+        shaped
+      >
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      v-model="drawerR"
+      app
+      clipped
+      right
+    >
+      <v-list>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+
+    <v-footer
+      app
+      height="72"
+      inset
+    >
+      <v-text-field
+        background-color="grey lighten-2"
+        dense
+        flat
+        hide-details
+        rounded
+        solo
+      ></v-text-field>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        drawer: null,
+        drawerR: null
+      }
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
